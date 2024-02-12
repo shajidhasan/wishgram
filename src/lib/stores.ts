2058fc7b0ef897422e7978b6fe189040fa22ed6c
shajidhasan <shajid6063@gmail.com>
2024-02-12 14:54:44 +0600

    made home page server rendered

--- src/lib/stores.ts
+++ src/lib/stores.ts
@@ -4,6 +4,7 @@ import normalFont from '$lib/assets/fonts/ChakraPetch-SemiBold.ttf'
 import highlightFont from '$lib/assets/fonts/bungee-shade-latin-400-normal.woff'
 import additionalFont from '$lib/assets/fonts/BungeeHairline-Regular.ttf'
 import TextToSVG from "./wishgram/text-to-svg"
+import { browser } from "$app/environment"
 
 export const processedSVGs = writable<ProcessedSVGs>()
 export const wishgramSettings = writable<WishgramSettings>({
@@ -29,8 +30,9 @@ export const loadFonts = async () => {
     const additionalBuffer = await additionalBlob.arrayBuffer()
 
     fonts.set({ normal: TextToSVG.parse(normalBuffer), highlight: TextToSVG.parse(highlightBuffer), additional: TextToSVG.parse(additionalBuffer) })
+    console.log("fonts loaded!")
 }
 
-if (!get(fonts)) {
+if (!get(fonts) && browser) {
     loadFonts()
 }
