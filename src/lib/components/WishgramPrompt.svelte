9ced9c08143c9145a7787c4f82b85e9c5134b7d5
shajidhasan <shajid6063@gmail.com>
2024-03-02 16:20:23 +0600

    fixed more details text input

--- src/lib/components/WishgramPrompt.svelte
+++ src/lib/components/WishgramPrompt.svelte
@@ -60,7 +60,7 @@
 
 			{#if moreOptions}
 				<div transition:slide class="flex flex-col gap-4 py-4">
-					<div class="flex w-full max-w-sm flex-col gap-1.5">
+					<div class="flex w-full flex-col gap-1.5">
 						<Label for="context">More details</Label>
 						<Input type="text" id="context" name="context"/>
 						<p class="text-sm text-muted-foreground">
